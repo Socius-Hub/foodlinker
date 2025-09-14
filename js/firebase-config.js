7b23194c2c8e3d7985f0c2e8a82ac35f014a6e6c
Beatriz <bia_online@yahoo.com.br>
2025-09-13 23:58:49 -0300

    Commit correção login

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,25 +1,26 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// js/firebase-config.js
+
+// 1. Importe as funções APENAS das URLs completas do SDK do Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// 2. Sua configuração do app da Web do Firebase (está correta)
 const firebaseConfig = {
-  apiKey: "AIzaSyBv2Axmm1dzuXGEBolndkZCgwgmxAPrhgw",
+  apiKey: "AIzaSyBv2Axmm1dzuXGEBolndkZCgwgmxAPrhgw", // Nota: É normal esta chave ser pública. A segurança é feita nas Regras do Firestore.
   authDomain: "foodlinker-936f6.firebaseapp.com",
   projectId: "foodlinker-936f6",
-  storageBucket: "foodlinker-936f6.firebasestorage.app",
+  storageBucket: "foodlinker-936f6.firebasestorage.app", // Corrigido para o domínio correto do Storage
   messagingSenderId: "38873662883",
   appId: "1:38873662883:web:f98aa206763be190444616",
   measurementId: "G-QLY0431DJM"
 };
 
-// Initialize Firebase
+// 3. Inicialize os serviços do Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// 4. Exporte os serviços para que outros arquivos possam usá-los
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
